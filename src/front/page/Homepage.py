e7546154e9a7828c54660a9f85e3bd14acddda1a
jonnyw <jonny_wong_@outlook.com>
2025-06-17 15:34:48 +0800

    [docs] update readme.md

--- src/front/page/Homepage.py
+++ src/front/page/Homepage.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 with st.container(border=True):
-    st.title("🎯 FineEval")
+    st.title("🎯 Fine-grained LLMs Evaluator")
     st.divider()
     st.subheader("Large Language Model Evaluation System")
-    st.markdown("##### Accurate, Efficient, and Scalable Evaluation for LLMs")
 
     st.info("👉 To get started, please select a task from the **sidebar menu** on the left.")
